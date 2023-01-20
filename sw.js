826c3d595c790c4f0eb17838729f877f4629889a
bittubro <116922532+bittubro@users.noreply.github.com>
2023-01-20 06:13:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,67 @@
 
+// Listen for the install event, which is triggered when the service worker is first registered
+
+self.addEventListener('install', event => {
+
+  // Create a new cache
+
+  event.waitUntil(
+
+    caches.open('my-site-cache')
+
+      .then(cache => {
+
+        // Add all the files from the web page to the cache
+
+        return cache.addAll([
+
+          '/',
+
+          '/index.html',
+
+          '/style.css',
+
+          '/script.js',
+
+          '/images/image1.jpg',
+
+          '/images/image2.jpg'
+
+        ]);
+
+      })
+
+  );
+
+});
+
+// Listen for the fetch event, which is triggered whenever a page requests a resource
+
+self.addEventListener('fetch', event => {
+
+  // Try to retrieve the requested resource from the cache
+
+  event.respondWith(
+
+    caches.match(event.request)
+
+      .then(response => {
+
+        // If the resource is in the cache, return it
+
+        if (response) {
+
+          return response;
+
+        }
+
+        // Otherwise, fetch the resource from the network
+
+        return fetch(event.request);
+
+      })
+
+  );
+
+});
+
